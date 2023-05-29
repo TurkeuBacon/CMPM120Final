7e145e7e24adc1be96e8df1f36f6a59a6792efa8
TurkeuBacon <99289161+TurkeuBacon@users.noreply.github.com>
2023-05-29 13:25:47 -0700

    Screen Fits
    Co-authored-by: dbustanUCSC <dbustanUCSC@users.noreply.github.com>

--- src/game.js
+++ src/game.js
@@ -1,15 +1,16 @@
 import MainMenu from "./scenes/MainMenu.js";
+import PresentDay from "./scenes/PresentDay.js";
 
 let config = {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
     type: Phaser.WEBGL,
-    width: 1280,
-    height: 720,
     backgroundColor: 0x00ffff,
     physics: {
         default: 'arcade',
 
     },
-    scene: [MainMenu],
+    scene: [MainMenu, PresentDay],
 }
 
 let game = new Phaser.Game(config);
