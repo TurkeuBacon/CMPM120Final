1af22791cbf5a160b0f9bd8ff8b2f69b7360db41
dbustanUCSC <dbustan@ucsc.edu>
2023-06-02 21:12:12 -0700

    Repurposed CameraManager, Created PlayerCamera

---
+++ src/PlayerCamera.js
@@ -0,0 +1,12 @@
+import Player from "./Player";
+
+class PlayerCamera {
+    constructor(scene, target){
+        this.scene = scene;
+        this.target = target;
+        this.scene.cameras.main.startFollow(this.target);
+        this.scene.cameras.main.setZoom(2);
+    }
+}
+
+export default PlayerCamera;
