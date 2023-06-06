fce42dd337b5e8f153f247a5c3765b6136cd3ce9
dbustanUCSC <dbustan@ucsc.edu>
2023-06-05 18:54:37 -0700

    Changes

---
+++ src/Map.js
@@ -0,0 +1,16 @@
+//Needs to hold map data (Image, group association)
+class Map {
+    constructor(mapName, mapImage, group){
+        this.mapName = mapName;
+        this.group = group;
+        this.mapImage = mapImage;
+    }
+    Vanish(){
+        
+    }
+    showMap(){
+
+    }
+}
+
+export default Map;
