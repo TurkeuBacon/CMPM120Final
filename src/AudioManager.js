d2eaf72516327bc9af05b205a186958d70540d87
Gordon Cai <gocai@ucsc.edu>
2023-06-07 14:32:11 -0700

    yes

---
+++ src/AudioManager.js
@@ -0,0 +1,53 @@
+class AudioManager{
+    constructor(){
+        this.audioObjects = [];
+    }
+    
+    loadAudio(key,file){
+        const audio = new Audio(file);
+        this.audioObjects[key] = audio;
+    }
+
+    playAudio(key){
+        const audio = this.audioObjects[key];
+        if(audio){
+            audio.play();
+        }
+    }
+
+    pauseAudio(key){
+        const audio = this.audioObjects[key];
+        if(audio){
+            audio.pause();
+        }
+    }
+
+    stopAudio(key){
+        const audio = this.audioObjects[key];
+        if(audio){
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    }
+
+}
+
+/*
+const audioManager = new AudioManager();
+
+// Load audio files
+audioManager.loadAudio('bgm', 'assets/audio/bgm.mp3');
+audioManager.loadAudio('sfx', 'assets/audio/sfx.wav');
+
+// Play audio
+audioManager.playAudio('bgm');
+
+// Pause audio
+audioManager.pauseAudio('bgm');
+
+// Stop audio
+audioManager.stopAudio('bgm');
+*/
+
+//extend the audiomanager class to whever you need it, then this is how you use it^^
+//create an audiomanager then use the commands.
