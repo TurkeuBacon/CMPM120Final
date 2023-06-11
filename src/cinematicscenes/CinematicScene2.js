cf70cfb258134333d3d1a540dbb8883fde979464
Gordon Cai <gocai@ucsc.edu>
2023-06-10 17:07:06 -0700

    completed Non-interactive cinematic

--- src/cinematicscenes/CinematicScene2.js
+++ src/cinematicscenes/CinematicScene2.js
@@ -46,17 +46,42 @@ class C2 extends Phaser.Scene{
 
         
         this.cameraManager = new CameraManager(this);
+        this.dialogueManager = new DialogueManager(this, 'dialogueBox');
+        let textBox = this.add.graphics();
+        textBox.fillStyle(0x000000, 0.8);  // Set the fill color and opacity
+            textBox.fillRect(315,400, 400, 100); 
+            textBox.depth = 2;
+        textBox.visible = false;
+        let testtext = this.add.text(325,425,
+            'This is John. He is a normal dude in a \nnormal village, living his normal life.').setScale(1);
+        testtext.depth = 2;
+        testtext.visible = false;
+        let purpletext = this.add.text(325,425,
+            'Oh shit!!! Purple guy is here!!! \nWatch out John!').setScale(1);
+        purpletext.depth = 2;
+        purpletext.visible = false;
+        let poisontext = this.add.text(325,425,
+            '"Hee Hee Heeee!!! I have poisoned you!\n There is no escape. \nGoodbye!"').setScale(1);
+        poisontext.depth = 2;
+        poisontext.visible = false;
+        let finaltext = this.add.text(325,425,
+            'John is now poisoned...\nYou must find a cure for him!').setScale(1);
+        finaltext.depth = 2;
+        finaltext.visible = false;
+
+
+
         const intro2 = this.sound.add('overworldBGM');
         const burplenurple = this.sound.add('leanguy');
         intro2.play();
         
-        this.PresentDayBG = this.add.image(screenWidth/2, screenHeight/2, 'presentDayFloor');
-        this.PresentDayBG.depth = 1;
-        this.PresentDayBG.alpha = 1;
+        let PresentDayBG = this.add.image(screenWidth/2, screenHeight/2, 'presentDayFloor');
+        PresentDayBG.depth = 1;
+        PresentDayBG.alpha = 1;
 
-        this.buildingPresent = this.add.image(screenWidth/2, screenHeight/2, 'presentDayHousing');
-        this.buildingPresent.depth = 4;
-        this.buildingPresent.alpha = 1;
+        let buildingPresent = this.add.image(screenWidth/2, screenHeight/2, 'presentDayHousing');
+        buildingPresent.depth = 4;
+        buildingPresent.alpha = 1;
 
         let player = this.add.sprite(0,345,'player');
       //  let purplehead = this.add.sprite(800,325,'head');
@@ -158,6 +183,8 @@ class C2 extends Phaser.Scene{
                 frames: this.anims.generateFrameNumbers('player', {start: 4, end: 7}),
                 repeat: -1
             });
+            
+
 
             player.play('walk_right');
         this.tweens.add({
@@ -165,10 +192,11 @@ class C2 extends Phaser.Scene{
             x:500,
             duration:6000,
             onComplete: function(){
-                stopAnim();
+                addText();
+                addBox();
             }
         });
-        this.tweens.add({
+        let purpleGuyComes = this.tweens.add({
             targets:this.purpleGuy,
             delay:10000,
             x:550,
@@ -176,18 +204,112 @@ class C2 extends Phaser.Scene{
             //onComplete: this.burplenurple.play(),
             onComplete: function () {
                 console.log("Tween completed");
+                stopAnim();
                 onEvent();
+                removeText();
+                addPurple();
+                purplePoison.resume();
+            }
+            
+        });
+        let purplePoison = this.tweens.add({
+            targets:this.purpleGuy,
+            delay:4000,
+            y:325,
+            x:525,
+            yoyo:true,
+            duration:4000,
+            paused:true,
+            onComplete: function(){
+                removePurple();
+                addPoison();
+                purpleLeave.resume();
             }
         });
+        let purpleLeave = this.tweens.add({
+            targets:this.purpleGuy,
+            delay:2000,
+            x:900,
+            duration:2000,
+            paused:true,
+            onComplete: function(){
+                removePoison();
+                burplenurple.stop();
+                intro2.play();
+                finaletext();
+                fade.resume();
+                fadebuilding.resume();
+            }
+        });
+        let fadebuilding = this.tweens.add({
+            delay:3000,
+            targets:buildingPresent,
+            alpha:0,
+            duration:5000,
+            paused:true,
+        });
+        let fade = this.tweens.add({
+            delay:3000,
+            targets:PresentDayBG,buildingPresent,
+            alpha:0,
+            duration:5000,
+            paused:true,
+            onComplete(){
+                fadeplayer.resume();
+            }
+        });
+        let fadeplayer = this.tweens.add({
+            delay:2000,
+            targets:player,
+            alpha:0,
+            paused:true,
+            onComplete:function(){
+                intro2.stop();
+                textfade.resume();
+            }
+        });
+        let textfade = this.tweens.add({
+            delay:2000,
+            targets:finaltext,
+            alpha: 0,
+            paused:true,
+        })
+       
        function onEvent(){
         burplenurple.play();
         intro2.stop();
-    }
+         }
         function stopAnim(){
             player.anims.stop();
         }
-        
-        
+        function addBox(){
+            textBox.visible = true;
+        }
+        function removeBox(){
+            textBox.visible = false;
+        }
+        function removeText(){
+            testtext.visible = false;
+        }
+        function addText(){
+            testtext.visible = true;
+        }
+        function addPurple(){
+            purpletext.visible = true;
+        }
+        function removePurple(){
+            purpletext.visible = false;
+        }
+        function addPoison(){
+            poisontext.visible = true;
+        }
+        function removePoison(){
+            poisontext.visible = false;
+        }
+        function finaletext(){
+            finaltext.visible = true;
+        }
     }
-}
+    }
+
 export default C2;
